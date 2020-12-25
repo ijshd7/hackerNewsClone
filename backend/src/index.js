1a3c6595c865c3f7d125e8feb6ca425961f8dc75
Isaiah Sherrill <ijshd7@mail.missouri.edu>
2020-12-25 01:11:10 -0600

    more changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,16 +5,39 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = `
   type Query {
     info: String!
+    feed: [Link!]!
   }
 `
+
+type Link {
+  id: ID!
+  description: String!
+  url: String!
+}
+
+// variable used to store links at runtime
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
 
 // 2
 // implementation
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
   }
 }
+
+
 
 // 3
 // schema and resolvers are passed to the server
